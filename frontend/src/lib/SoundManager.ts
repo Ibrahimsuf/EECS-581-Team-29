ad09afbb61157ead5a4431ed11a7a81538c9e780
AbhiroopGoel <157642305+AbhiroopGoel@users.noreply.github.com>
2025-09-30 11:03:28 -0500

    Added SoundManager.ts

---
+++ frontend/src/lib/SoundManager.ts
@@ -0,0 +1,21 @@
+// frontend/src/SoundManager.js
+class SoundManager {
+  constructor() {
+    this.sounds = {
+      explosion: new Audio("/sounds/explosion.mp3"),    //initializing the sounds with names
+      flag: new Audio("/sounds/flag.mp3"),
+      gameStart: new Audio("/sounds/game_start.mp3"),
+      victory: new Audio("/sounds/victory.mp3"),
+    };
+  }
+
+  play(name) {
+    if (this.sounds[name]) {
+      this.sounds[name].currentTime = 0; // restart if already playing
+      this.sounds[name].play();
+    }
+  }
+}
+
+const soundManager = new SoundManager();
+export default soundManager;
